aa37a5897d469e5fa4da1087c81154fc34c7b474
aajy <aajy.000@gmail.com>
2025-06-26 11:48:57 +0900

    feat: 메인화면 레이아웃

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div>
-      <div>logo</div>
-      <div>
-        right
-        <Link href="/login">Login</Link>
-      </div>
-    </div>
-  );
+    <main>
+      홈페이지
+    </main>
+  )
 }
