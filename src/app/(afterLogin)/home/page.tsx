f549ebc537a92ba5377f95d4b41570b185151345
aajy <aajy.000@gmail.com>
2025-07-07 01:39:12 +0900

    Feat: 홈 탭메뉴 contextAPI사용

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,20 @@
+import Post from "../_component/Post";
+import PostForm from "./_component/PostForm";
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+
 export default function Home() {
   return (
     <main>
-      홈페이지
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
     </main>
-  )
+  );
 }
